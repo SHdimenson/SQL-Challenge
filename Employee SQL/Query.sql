17f85c34746f91f55f4ba443744e9cbcd3e0913e
SHdimenson <73255272+SHdimenson@users.noreply.github.com>
2021-01-23 23:20:14 -0500

    Commit 2

---
+++ Employee SQL/Query.sql
@@ -0,0 +1,8 @@
+-- Verifying the import/data-load into tables.
+SELECT * FROM employees;
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
